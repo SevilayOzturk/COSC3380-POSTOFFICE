9f099e0bce67a3f3625cd3ceb464452e75c74bc6
Sevilay Ozturk <osevilay@gmail.com>
2022-04-02 03:38:20 -0500

    Added Employees CRUD

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3000;
 
+app.use(cors());
+app.use(express.json());
 
-const app=express();
+//Routes Includes
+const customersRouter = require('./routes/customers');
+const employeesRouter = require('./routes/employees');
 
+//Routes
+app.use('/app/customers', customersRouter);
+app.use('/app/employees', employeesRouter);
 
-
-
-app.listen(() =>{
-    console.log("server started at uhpost.xyz");
+app.use ((err, req, res, next) => {
+    const code = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(code).json({message:err.message});
+    return;
 });
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:'Welcome to the Backend'
+    });
+});
+
+
+app.listen(port, ()=>console.log(`Server is running on port ${port}`));
