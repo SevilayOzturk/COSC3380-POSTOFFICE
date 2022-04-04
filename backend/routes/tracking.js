9b90a7a0238814248363955859e2983cc22854cc
Sevilay Ozturk <osevilay@gmail.com>
2022-04-03 22:09:42 -0500

    Routers and Services are Finished

---
+++ backend/routes/tracking.js
@@ -0,0 +1,75 @@
+//Express
+const express = require('express');
+const cors = require('cors');
+const router = express.Router();
+
+const trackingsService = require('../services/trackings-service');
+const postoffices = require('../services/postoffice')
+router.get('/', async function(req,res,next) {
+    try {
+        res.json('trackings');
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/all-trackings', async function(req,res,next) {
+    try {
+        res.json(await trackingsService.get(req.query.page));
+        } catch (err) {
+            console.error(`Error `, err.message);
+            next(err);
+
+        }   
+});
+
+router.get('/all', async function(req,res,next) {
+    try {
+        res.json(await trackingsService.getData());
+        } catch (err) {
+            console.error(`Error `, err.message);
+            next(err);
+        }
+});
+
+router.get('/userid/:id', async function(req,res,next) {
+    let id = req.params.id;
+    console.log('ID: ${id}',id);
+    try {
+        res.json(await trackingsService.getById(id));
+        } catch (err) {
+            console.error(`Error `, err.message);
+            next(err);
+        }
+});
+
+router.post('/createtracking', async function(req,res,next) {
+    try {
+        res.json(await trackingsService.createtracking(req));
+        } catch (err) {
+            console.error(`Error `, err.message);
+            next(err);
+            }
+});
+
+router.put('/updatetracking/:id', async function(req,res,next) {
+    try {
+        res.json(await trackingsService.updatetracking(req));
+        } catch (err) {
+            console.error(`Error `, err.message);
+            next(err);
+            }
+});
+
+router.delete('/removetracking/:id', async function(req,res,next) {
+    try {
+        res.json(await trackingsService.removetracking(req));
+        } catch (err) {
+            console.error(`Error `, err.message);
+            next(err);
+            }
+});
+
+                  
+
+module.exports = router;
---
+++ backend/services/item-service.js
@@ -0,0 +1,84 @@
+const db = require('./db');
+const helper = require('../helper');
+const config = require('../config');
+
+async function get() {
+    const rows = await db.query(`SELECT * FROM Items`);
+    const data = helper.rowclean(rows);
+    return data;
+}
+
+async function getItems(id) {
+    const rows = await db.query(`SELECT * FROM Items WHERE Item_ID = ?`, [id]);
+    const data = helper.rowclean(rows);
+    return data;
+}
+
+async function getData(page = 1){
+    const offset = helper.getoffset(page, config.listPerPage);
+    const rows = await db.query(`select * from Items LIMIT ${offset},${config.listPerPage}`);
+    const data = helper.rowclean(rows);
+    const meta = {page};
+    return {meta, data}
+}
+
+async function getById(Item_ID) {
+    const user = await db.query(`SELECT * FROM Items WHERE Item_ID = ?`, [Item_ID]);
+    const data = helper.rowclean(user);
+    // const meta = {page};
+    // return {meta, data};
+    return data;
+}
+
+async function createItems(req) {
+    const PO_ID = req.body.PO_ID;
+    const Item_ID = req.body.Item_ID;;
+    const Item_Name = req.body.Item_Name;
+    const Item_Count = req.body.Item_Count;
+    const Item_Cost= req.body.Item_Cost; 
+    
+    console.log("CreateItems", PO_ID, Item_ID, Item_Name, Item_Count, Item_Cost);
+
+    const Items = await db.query(
+        `INSERT INTO Items (PO_ID, Item_ID, Item_Name, Item_Count, Item_Cost) 
+        VALUES (?,?,?,?,?)`, 
+        [PO_ID, Item_ID, Item_Name, Item_Count, Item_Cost]);
+
+   
+
+    console.log("CreateItem:", Items);
+    return {"message": "Item created"};
+}
+
+async function updateItems(req) {
+    const PO_ID = req.body.PO_ID;
+    const Item_Name = req.body.Item_Name;
+    const Item_Count = req.body.Item_Count;
+    const Item_Cost= req.body.Item_Cost;
+
+    console.log("Update Items:", PO_ID, Item_Name, Item_Count, Item_Cost);
+
+    const Items = await db.query(
+        `UPDATE Items SET PO_ID=?, Item_Name=?, Item_Count=?, Item_Cost=? WHERE Item_ID = ?`,
+        [PO_ID, Item_Name, Item_Count, Item_Cost, req.params.id]);
+    
+        return("message", "Item updated");
+    
+}
+
+async function removeItems(req) {
+    let id = req.body.id;
+
+    const remove = await db.query(`DELETE FROM Items WHERE Item_ID = ?`, [id]);
+    return {"message": "Item deleted"};
+}
+
+module.exports = {
+    get,
+    getItems,
+    getData,
+    getById,
+    createItems,
+    updateItems,
+    removeItems
+}
